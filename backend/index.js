2636b446f326748ab8605b326f656da43a8392f2
Dawid Klatkiewicz <klatkiewiczdawid@holinhed.com>
2018-12-26 12:41:12 -0500

    init

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const express = require('express');
+const cors = require('cors');
+const check = require('./modules/check');
+
+const PORT = 5000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/:id', (req, res) => {
+    res.status(200);
+    res.json({
+        message: 'hello world'
+    })
+});
+
+app.post('/new', (req, res) => {
+    res.status(200);
+    res.json({
+        message: 'hello world'
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
+});
